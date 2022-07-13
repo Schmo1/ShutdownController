b48bc501b808eb34482ea17691b0cb1c9720572d
David Schmoranz <david.schmoranz@outlook.com>
2022-07-13 21:24:36 +0200

    Cartesian Chart added

---
+++ ShutdownController/Utility/DownUploadController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using ShutdownController.Utility;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace ShutdownController.Utility
+{
+    internal class DownUploadController
+    {
+    }
+}
--- ShutdownController/ViewModels/DownUploadViewModel.cs
+++ ShutdownController/ViewModels/DownUploadViewModel.cs
@@ -1,9 +1,44 @@
-﻿using ShutdownController.Core;
+﻿using LiveCharts;
+using ShutdownController.Core;
 
 namespace ShutdownController.ViewModels
 {
     public class DownUploadViewModel : ObservableObject
     {
+
+        private int scaleMax;
+        private ChartValues<double> downloadValues;
+        private ChartValues<double> uploadValues;
+
+
+        public int ScalaMax
+        {
+            get { return scaleMax; }
+            set { scaleMax = value; OnPropertyChanged(); }
+        }
+
+        public ChartValues<double> DownloadValues
+        {
+            get { return downloadValues; }
+            set { downloadValues = value; OnPropertyChanged(); }
+        }
+        public ChartValues<double> UploadValues
+        {
+            get { return uploadValues; }
+            set { uploadValues = value; OnPropertyChanged(); }
+        }
+
+
+
+
+
+
+        public DownUploadViewModel()
+        {
+            ScalaMax = 100;
+            DownloadValues = new ChartValues<double> { 23,23,2,3,1,10,50,40};
+            UploadValues = new ChartValues<double> { 20, 10, 20, 30, 30, 10, 20, 20, 20, 20, 30, 30, 50, 20, 32, 30, 50, 80, 80, 70, 80, 54, 54, 30, 57, 40, 40, 80, 93, 95, 85 };
+        }
 
     }
 }
